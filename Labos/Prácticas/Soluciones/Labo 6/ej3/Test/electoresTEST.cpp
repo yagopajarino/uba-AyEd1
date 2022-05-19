1101fbde94ec17a35a124cb4418c601a5d3feb6a
Yago Pajariño <yago977@hotmail.com>
2022-05-19 16:14:06 -0300

    Adds resoluciones labo 6

--- Labos/Prácticas/Soluciones/Labo 6/ej3/Test/electoresTEST.cpp
+++ Labos/Prácticas/Soluciones/Labo 6/ej3/Test/electoresTEST.cpp
@@ -1,4 +1,42 @@
 #include "../electores.h"
 #include "gtest/gtest.h"
 
-// Escribir tests aca:
+TEST(electores, YagoEdad) {
+    int res = validarVotante(1997,8,11);
+    EXPECT_EQ(res, 2);
+}
+
+TEST(electores, noVota) {
+int res = validarVotante(2021,1,25);
+EXPECT_EQ(res, 0);
+}
+
+TEST(electores, opcionalMenor) {
+    int res = validarVotante(2005,1,13);
+    EXPECT_EQ(res, 1);
+}
+
+TEST(electores, obligado) {
+    int res = validarVotante(1970,12,25);
+    EXPECT_EQ(res, 2);
+}
+
+TEST(electores, opcionalMayor) {
+    int res = validarVotante(1940,2,4);
+    EXPECT_EQ(res, 3);
+}
+
+TEST(electores, fechaInvalidaAño) {
+    int res = validarVotante(2150,2,4);
+    EXPECT_EQ(res, 4);
+}
+
+TEST(electores, fechaInvalidaMes) {
+    int res = validarVotante(1980,13,4);
+    EXPECT_EQ(res, 4);
+}
+
+TEST(electores, fechaInvalidaDia) {
+    int res = validarVotante(1980,13,33);
+    EXPECT_EQ(res, 4);
+}
--- Labos/Prácticas/Soluciones/Labo 6/ej3/electores.cpp
+++ Labos/Prácticas/Soluciones/Labo 6/ej3/electores.cpp
@@ -1,6 +1,53 @@
 #include "electores.h"
+#include <vector>
+
+using namespace std;
+
+float deltaEleccion(int mes, int dia) {
+    int diasTotales = 365;
+    return float (9 + 30 + 31) / float (diasTotales);
+}
+
+float fracCumplida(int mes, int dia) {
+    int diasTotales = 365;
+    int diasCumplidos = 0;
+    vector<int> meses = {31,28,31,30,31,30,31,31,30,31,30,31};
+    mes --;
+    for (int i = mes + 1; i < 12; ++i) {
+        diasCumplidos += meses[i];
+    }
+    diasCumplidos += meses[mes] - dia;
+    return float(diasCumplidos) / float(diasTotales);
+}
+
+float getEdad(int a, int m, int d) {
+    float años = 2022 - a;
+    años += fracCumplida(m,d);
+    años -= deltaEleccion(m,d);
+    return años;
+}
+
+bool fechaInvalida(int a, int m, int d) {
+    if ( a > 2022 || m > 12 || m < 1 || d < 1 || d > 31 ) {
+        return 4;
+    }
+}
 
 int validarVotante(int a, int m, int d) {
-	return NO_VOTA;
-	// Borrar el return dummy y completar
+    if ( fechaInvalida(a, m ,d) ) {
+        return 4;
+    }
+    int res = 3;
+    float edad = getEdad(a,m,d);
+    if (edad < 0){
+        res = 4;
+    }
+    else if (edad < 16.0 ) {
+        res = 0;
+    } else if ( (16.0 <= edad) && (edad < 18.0) ) {
+        res = 1;
+    } else if ( (18.0 <= edad) && (edad < 70.0) ) {
+        res = 2;
+    }
+    return res;
 }
